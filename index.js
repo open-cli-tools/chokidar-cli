459a14991dc6040af2b0afc412cea0f748a33fc9
Ankur Oberoi <aoberoi@gmail.com>
2018-12-22 15:19:34 -0800

    fix an import by deleting the .default property
    based on: https://github.com/kimmobrunfeldt/chokidar-cli/pull/29#pullrequestreview-56586602

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var Promise = require('bluebird');
 var _ = require('lodash');
 var chokidar = require('chokidar');
-var spawn = require('npm-run-all/lib/spawn').default;
+var spawn = require('npm-run-all/lib/spawn');
 
 var EVENT_DESCRIPTIONS = {
     add: 'File added',
